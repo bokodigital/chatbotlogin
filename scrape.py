9a7948e39b5007a2843756e4ba7f09a492c4ab13
bokodigital <bokodigital@gmail.com>
2024-12-16 22:59:00 +0530

    changes

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnecti
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-SBR_WEBDRIVER = 'https://brd-customer-hl_7f91e4b7-zone-scraping_browser3:hp4vqoi64ygp@brd.superproxy.io:9515'
+SBR_WEBDRIVER = 'https://brd-customer-hl_c890647c-zone-scraping_browser1:he6dmy54k7fw@brd.superproxy.io:9515'
 
 def scrape_website(url):
     print("Launching from browser..")
